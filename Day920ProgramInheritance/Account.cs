18cdd77f0110114958a511fdc1d84a54e89dfb4f
ESchulstad <eschulstad@gmail.com>
2016-11-07 20:46:39 -0500

    initial commit

---
+++ Day920ProgramInheritance/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day920ProgramInheritance
+{
+    class Account
+    {
+
+        public virtual int Deposit(int money)
+        {
+            int accountBalance = 1000;
+            accountBalance += money;
+            return accountBalance;
+        }
+
+        public virtual int Withdraw(int money)
+        {
+            int accountBalance = 1000;
+            accountBalance -= money;
+            return accountBalance; 
+        }
+
+    }
+}
---
+++ Day920ProgramInheritance/CheckingAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day920ProgramInheritance
+{
+    class CheckingAccount : Account
+    {
+
+        public override int Deposit(int money)
+        {
+            return base.Deposit(money);
+        }
+    }
+}
